834cb6d8b6057e1266c1638748687c807b4b8a1a
Yu-xinz <zhangshzh@shanghaitech.edu.cn>
2024-10-03 00:16:35 +0800

    First version

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Link, Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      &copy; {new Date().getFullYear()} Takuya Matsuyama. All Rights Reserved.
+      &copy; {new Date().getFullYear()} Shizhen Zhang. Powered by&nbsp;
+      <Link
+        as={NextLink}
+        href="https://github.com/craftzdog/craftzdog-homepage"
+        passHref
+        target="_blank"
+        color="inherit"
+      >
+        Takuya Matsuyama
+      </Link>.
     </Box>
   )
 }
