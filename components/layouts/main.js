63c545607b1b4abeefb1eff44d45c78447a4a315
Yu-xinz <zhangshzh@shanghaitech.edu.cn>
2024-10-04 21:46:08 +0800

    change name

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,18 +15,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
+        <meta name="description" content="Yuxinz's homepage" />
+        <meta name="author" content="Yuxinz" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Yuxinz" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="Yuxinz" />
+        <meta name="og:title" content="Yuxinz" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Shizhen Zhang - Homepage</title>
